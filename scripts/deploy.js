3f1cb6eff1e92f4df45e25488ff0e9f8aee3b715
sidereior <alexanderlnanda@gmail.com>
2022-11-05 17:51:46 -0400

    fixed deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,8 +1,23 @@
+const hre = require("hardhat");
+
 async function main() {
-    const nftContractFactory = await hre.ethers.getContractFactory("BitAppsNFT");
-    const nftContract = await nftContractFactory.deploy();
-    await nftContract.deployed();
-    console.log("Contract deployed to:", nftContract.address);
+  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
+  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
+  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+
+  const lockedAmount = hre.ethers.utils.parseEther("1");
+
+  const Lock = await hre.ethers.getContractFactory("Safe");
+  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+
+  await lock.deployed();
+
+  console.log(
+    `Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
+  );
 }
 
-main();
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
